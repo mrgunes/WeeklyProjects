ae857d5510dd93b6abc35b00ccebc314f7e9f097
Eyyup Eric Gunes <31079039+mrgunes@users.noreply.github.com>
2020-12-08 03:26:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//Body Mass Index:(BMI)
+
+var massMark = 83 ; 
+var massJohn = 110; 
+var heightMark = 1.75;
+var heightJohn = 2;
+
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+var BMIMark = massMark / (heightMark * heightMark); 
+
+console.log(BMIJohn);
+console.log(BMIMark);
+
+if (BMIJohn > BMIMark) { 
+    console.log('BMIJohn is higher than BMIMark'); 
+    } else { 
+         console.log('BMIMark is lower than BMIJohn');
+         };
+// burak harikasin dostum
