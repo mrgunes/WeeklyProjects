0d7aa76f566dcc5b46cf2fda6db1f8cbb4eef098
Eyyup Eric Gunes <31079039+mrgunes@users.noreply.github.com>
2020-12-08 03:28:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-//Body Mass Index:(BMI)
 
-var massMark = 83 ; 
-var massJohn = 110; 
-var heightMark = 1.75;
-var heightJohn = 2;
 
-var BMIJohn = massJohn / (heightJohn * heightJohn);
-var BMIMark = massMark / (heightMark * heightMark); 
 
-console.log(BMIJohn);
-console.log(BMIMark);
 
-if (BMIJohn > BMIMark) { 
-    console.log('BMIJohn is higher than BMIMark'); 
-    } else { 
-         console.log('BMIMark is lower than BMIJohn');
-         };
-// burak harikasin dostum
+
+
+
+//const calculateCharacter = () =>{
+var tweet=" Enrich your work. You can add a name prompt and use string interpolation to see your name in alert result.";
+var tweetCount = tweet.length ;
+var tweetLeft  = 150 - tweetCount
+console.log(`You have written ${tweetCount} characters, you have ${tweetLeft} charecters remaining.`);
+//}
+
+
+// Invoke the function when button is clicked
+
+//btn.addEventListener('click', calculateCharacter);
+
+
+
+//var tweet = "use string interpolation to see your name in alert result.";
+//var tweetCount = tweet.length ;
+//var tweetLeft  = 150 - tweetCount ;
+
+//console.log(tweetLeft);
